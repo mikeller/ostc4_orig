bba446253728dbb626572f1fb61363bb680b1130
ideenmodellierer <ideenmodellierer@localhost>
2019-06-17 19:46:18 +0200

    Reworked detection function The first implementation was not stable enough during underwater tests => redesign taking speed deltas into account

--- Discovery/Inc/motion.h
+++ Discovery/Inc/motion.h
@@ -14,18 +14,16 @@ typedef enum
 {
 		DETECT_START = 0,
 		DETECT_POS_MOVE,
-		DETECT_NEG_MOVE,
 		DETECT_MAXIMA,
-		DETECT_MINIMA,
 		DETECT_FALLBACK,
-		DETECT_RISEBACK,
 		DETECT_POS_SHAKE,
+		DETECT_NEG_MOVE,
+		DETECT_MINIMA,
+		DETECT_RISEBACK,
 		DETECT_NEG_SHAKE,
 		DETECT_NOTHING
 } detectionState_t;
 
-
-void InitMotion(void);
 detectionState_t detectShake(float currentPitch);
 
 #endif /* INC_MOTION_H_ */
