64e69baa944c582dd1360588d523d3620b5b83f0
ideenmodellierer <ideenmodellierer@localhost>
2019-06-17 19:53:01 +0200

    Bugfix: selection of current view not working

--- Discovery/Src/t7.c
+++ Discovery/Src/t7.c
@@ -1481,7 +1481,7 @@ void t7_set_customview_to_primary(void)
 
 void t7_change_customview(uint8_t action)
 {
-    const uint8_t *pViews;
+    uint8_t *pViews;
     uint8_t *pStartView,*pCurView, *pLastView;
     _Bool cv_disabled = 0;
 
@@ -1515,34 +1515,36 @@ void t7_change_customview(uint8_t action)
     }
     else
     {
-		pViews--;
-		if(pViews = pStartView)
+		if(pViews == pStartView)
 		{
 			pViews = pLastView - 1;
 		}
+		else
+		{
+			pViews--;
+		}
     }
-    if(stateUsed->mode == MODE_DIVE)
-    {
-        do
-        {
-            cv_disabled = 0;
-            for(int i=0;i<6;i++)
-            {
-                if((*pViews == cv_changelist[i]) && !CHECK_BIT_THOME(settingsGetPointer()->cv_configuration, cv_changelist[i]))
-                {
-                    cv_disabled = 1;
-                    break;
-                }
-            }
 
-            if ((*pViews == CVIEW_sensors || *pViews == CVIEW_sensors_mV) &&
-            	stateUsed->diveSettings.ppo2sensors_deactivated)
-            {
-            	cv_disabled = 1;
-            }
+       do
+       {
+           cv_disabled = 0;
+           for(int i=0;i<6;i++)
+           {
+               if((*pViews == cv_changelist[i]) && !CHECK_BIT_THOME(settingsGetPointer()->cv_configuration, cv_changelist[i]))
+               {
+                   cv_disabled = 1;
+                   break;
+               }
+           }
 
-            if(cv_disabled)		/* view is disabled => jump to next view */
-            {
+           if (((*pViews == CVIEW_sensors) || (*pViews == CVIEW_sensors_mV)) &&
+           	((stateUsed->diveSettings.ppo2sensors_deactivated) || (stateUsed->diveSettings.ccrOption == 0)))
+           {
+	           	cv_disabled = 1;
+           }
+
+           if(cv_disabled)		/* view is disabled => jump to next view */
+           {
             	if((action == ACTION_BUTTON_ENTER) || (action == ACTION_SHAKE_POS))
             	{
             		pViews++;
@@ -1553,10 +1555,13 @@ void t7_change_customview(uint8_t action)
             	}
             	else
             	{
-            		pViews--;
             		if(pViews == pStartView)
             		{
             			pViews = pLastView - 1;
+            		}
+            		else
+            		{
+            			pViews--;
             		}
             	}
             }
