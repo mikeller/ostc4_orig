a1e96021a6626a6cfe5d9b72ab510157128eafa9
Michael Keller <github@ike.ch>
2021-07-20 23:01:36 +1200

    Made release build build cleanly.

--- Discovery/Src/gfx_fonts.c
+++ Discovery/Src/gfx_fonts.c
@@ -43,5 +43,23 @@
 #include "Fonts/image_battery.h"
 #include "Fonts/image_heinrichs_weikamp.h"
 #include "Fonts/image_ostc.h"
+#else
+
+/* Fonts fixed in upper region */
+const tFont Awe48 __attribute__ (( used, section(".upper_font_directory.Awe48") ));
+const tFont FontT24 __attribute__ (( used, section(".upper_font_directory.FontT24") ));
+const tFont FontT42 __attribute__ (( used, section(".upper_font_directory.FontT42") ));
+const tFont FontT48 __attribute__ (( used, section(".upper_font_directory.FontT48") ));
+const tFont FontT54 __attribute__ (( used, section(".upper_font_directory.FontT54") ));
+const tFont FontT84 __attribute__ (( used, section(".upper_font_directory.FontT84") ));
+const tFont FontT84Spaced __attribute__ (( used, section(".lower_font_directory.FontT84Spaced") ));
+const tFont FontT105 __attribute__ (( used, section(".upper_font_directory.FontT105") ));
+const tFont FontT144 __attribute__ (( used, section(".upper_font_directory.FontT144") ));
+
+/* Images fixed in upper region */
+#include "Fonts/image_battery.h"
+#include "Fonts/image_heinrichs_weikamp.h"
+#include "Fonts/image_ostc.h"
+
 
 #endif // INCLUDE_FONTS_BINARY
